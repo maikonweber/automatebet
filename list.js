3e28479c0052babe6867206977d7276a3f39a2c0
Maikon <m.carvalho@grouplinknetwork.com>
2022-04-26 17:28:43 -0300

    add all

--- list.js
+++ list.js
@@ -13,7 +13,33 @@ const redis = require('redis');
   await subscriber.connect();
 
     await subscriber.subscribe('roulleteEventsTelegram', (message) => {
-    console.log(message); // 'message'
+        
+      let regEx = /Possível Entrada/g;
+      let regEx2 = /Abortar/g;
+      let regEx3 = /Entrada/g;
+      let regEx4 = /Green/g;
+      let regEx5= /Red/g;
+      let regEx6 = /Vamos para o 1° Gale/g;
+      
+      if (regEx.test(message)) {
+        console.log(message, "regEx");
+      }
+      if (regEx2.test(message)) {
+        console.log(message, "regEx2");
+      }
+      if (regEx3.test(message)) {
+        console.log(message, "regEx3");
+      }
+      if (regEx4.test(message)) {
+        console.log(message, "regEx4");
+      }
+      if (regEx5.test(message)) {
+        console.log(message, "regEx5");
+      }
+      if (regEx6.test(message)) {
+        console.log(message, "regEx6");
+      }
+
     });
 
 })();
