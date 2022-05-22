bf5ba2db13c554275a56ccca3b1065c33187b7d4
unknown <Maikon Weber>
2022-05-22 12:30:18 -0300

    add

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ app.use(cors(
 
 
 app.post('/api/v2/setblaze', async (req, res) => {
+  console.log(req)
   const horario = req.body.horario
   const valor = req.body.valor
   const username = req.body.username
