e6999fe90b55881999c8359801ecb3c4eb45069c
unknown <Maikon Weber>
2022-06-01 22:45:18 -0300

    ad

--- database.js
+++ database.js
@@ -235,7 +235,7 @@ async function getLastNumber (name) {
 }
 
 async function InsertRoullete (name, numberJson, jsonPreload, jsonbStrategy) {
-    let sql = `insert into robotbetpayload (name, numberjson, jsonpreload, jsonbstrategy) 
+    let sql = `insert into robotbetpayload (name, numberjson, jsonbpreload, jsonbstrategy) 
     values ($1, $2, $3, $4, $5)`;
 
     let result = await pool.query(sql, [name, numberJson, jsonPreload, jsonbStrategy]);
