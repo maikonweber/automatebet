9ee86ecd354e51bd13053adfc400c50a54c9415a
Maikon <maikonweber@gmail.com>
2022-06-03 21:25:36 +0000

    add

--- telegram.js
+++ telegram.js
@@ -73,10 +73,10 @@ const obj = {
 
 async function clientSendMsgGrupo (client, grupoId, mensagem) {
      const sala = await client.getInputEntity(grupoId)
-     
+     console.log(sala) 
      return   client.invoke(new Api.messages.SendMessage({
                           peer: sala,
-                          message: chat.message.toString()
+                          message: mensagem.toString()
                       }))
 
 }
