9412161ed5092895bd92fa9e0862d26758050f57
Maikon <m.carvalho@grouplinknetwork.com>
2022-06-29 14:21:06 -0300

    removing

--- consumerRoletaProcced.js
+++ consumerRoletaProcced.js
@@ -270,7 +270,7 @@ function colunasAlternat(json) {
                 // Make a post with axios for localhost:3000/api/bet365
                 console.log('Send Request')
           
-                axios.post('https://api.muttercorp.online/api/cards', obj).then(
+                axios.post('https://api.muttercorp.online/api/bet365', obj).then(
                         res => {
                            console.log(res.data);
                         }
