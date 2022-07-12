12e6907c19ddc1597f29e55f5d8b976372e5fb7a
maikonweber@gmail.com <maikonweber@gmail.com>
2022-07-12 20:29:40 -0300

    add

--- jsonObjects/strategy.js
+++ jsonObjects/strategy.js
@@ -29,6 +29,12 @@ const expectNumber = {
          const col3 = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36]
          return col3
     },
+    'Alternar colunas 3 e 2' : function () {
+     const col3 = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36]
+     const col2 = [2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35]
+     const col1 = [1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34]
+     return this["Alternancia da Coluna 3 e 1"]
+},
     'Alternancia da Coluna 3 e 2' : function () {
          const col3 = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36]
          const col2 = [2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35]
