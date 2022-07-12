33db054650460d7fd9bd7bdab134991aa9d9dbf7
maikonweber@gmail.com <maikonweber@gmail.com>
2022-07-12 14:09:07 -0300

    ad

--- server.js
+++ server.js
@@ -80,9 +80,8 @@ app.get('/exportcsv', async (req, res) => {
   }
     
    await worksheet.xlsx.writeFile('export2.xlsx')
-  const file = `${__dirname}/export2.xlsx`
-    console.log(xlsx)
-    res.attachment('roleta.xlsx').send(file)
+    const file = `${__dirname}/export2.xlsx`
+   await res.attachment('roleta.xlsx').send(file)
   })
 
 
