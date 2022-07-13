df962cf4d20420f9d0163654c6598206b6becc1a
Maikon <m.carvalho@grouplinknetwork.com>
2022-07-13 11:26:56 -0300

    add

--- jsonObjects/strategy.js
+++ jsonObjects/strategy.js
@@ -3,43 +3,43 @@ const expectNumber = {
          const col1 = [1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34]
          const col2 = [2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35]
          const col3 = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36]
-         return col3
+         return col1.concat(col2)
     },
     'Alternancia da Coluna 3 e 1' : function () {
          const col3 = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36]
          const col1 = [1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34]
          const col2 = [2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35]
-         return col2
+         return col3.concat(col1)
     },
     'Alternancia da Coluna 1 e 3' : function () {
          const col1 = [1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34]
          const col3 = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36]
          const col2 = [2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35]
-         return col2
+         return col1.concat(col3)
     },
     'Alternancia da Coluna 3 e 1' : function () {
          const col3 = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36]
          const col1 = [1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34]
          const col2 = [2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35]
-         return col2
+         return col3.concat(col3)
     },
     'Alternancia da Coluna 2 e 1' : function () {
          const col2 = [2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35]
          const col1 = [1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34]
          const col3 = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36]
-         return col3
+         return col2.concat(col1)
     },
     'Alternar colunas 3 e 2' : function () {
      const col3 = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36]
      const col2 = [2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35]
      const col1 = [1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34]
-     return this["Alternancia da Coluna 3 e 1"]
+     return 
 },
     'Alternancia da Coluna 3 e 2' : function () {
          const col3 = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36]
          const col2 = [2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35]
          const col1 = [1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34]
-         return this["Alternancia da Coluna 3 e 1"]
+         return col3.concat(col2)
     },
     'Impar Reapeat' : function () {
          const impar = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35]
